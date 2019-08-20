6659f2e57b7d396a8a901fd654612966fb5514b3
mateusgpontes <mateus.gpontes@hotmail.com>
2019-08-20 15:28:28 -0300

    create user interaction

--- main.c
+++ main.c
@@ -3,13 +3,26 @@
 int main(){
 
     char nome[10] = "";
-    int chute;
+    int kick;
+    int secret_number = 60;
 
-    printf("Qual seu nome?\n");
+    printf("Qual seu nome? ");
     scanf("%s", nome);
-    printf("\n");
-
-    printf("Bem vindo ao jogo da Adivinhação %s\n", nome);
+    printf("\nBem vindo ao jogo da Adivinhação %s \n", nome);
+    
+    printf("Escolha um número de 0 a 100: ");
+    scanf("%i", &kick);
+    printf("\nSeu chute foi %i \n", kick);
+    
+    if(kick == secret_number){
+        return printf("Parabens você ganhou :D \n");
+    }
+    else if(kick < secret_number){
+        return printf("Você errou, o número secreto é maior \n");
+    }
+    else if(kick > secret_number){
+        return printf("Você errou, o número secreto é menor \n");
+    }
 
     return 0;
 }
